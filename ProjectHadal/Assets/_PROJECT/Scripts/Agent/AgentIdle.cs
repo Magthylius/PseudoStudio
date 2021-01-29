fa2e7eabe8c2aa4f73af3f61d088d9d9958a9604
NicholasTiew <nicholastiewofficial@gmail.com>
2021-01-29 16:34:25 +0800

    Revert "Revert "AI Agent""
    This reverts commit 4a57bb70094a53241d48541fb0000b4251397956.

---
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/AgentIdle.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace NicholasAI.GGJAI
+{
+    public class AgentIdle : MonoBehaviour
+    {
+        GameObject agentAnimal;
+        [SerializeField] GameObject[] mapPlanes;
+
+        [SerializeField] bool agentChangeRot;
+        [SerializeField] float agentTimer;
+        [SerializeField] float agentChoosePlaneTimer;
+        Vector3 destination;
+        NavMeshAgent agent;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            agentAnimal = gameObject;
+            agent = gameObject.GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (agentChangeRot == false)
+            {
+                transform.position -= new Vector3(0.0f, 0.0f, 0.1f);
+            }
+ 
+            else
+            {
+                transform.position += new Vector3(0.0f, 0.0f, 0.1f);
+            }
+                
+
+            agentTimer -= Time.deltaTime;
+            agentChoosePlaneTimer -= Time.deltaTime;
+
+            if (agentTimer < 0.0f)
+            {
+                agentTimer = 2.0f;
+                float RNGROTATION = Random.Range(0.0f, 0.1f);
+                if(RNGROTATION == 0.0f)
+                    agentChangeRot = !agentChangeRot;
+                
+            }
+
+            if(agentChoosePlaneTimer < 0.0f)
+            {
+                agentChoosePlaneTimer = Random.Range(3.0f, 10.0f);
+                ChoosePlane();
+            }
+      
+
+        }
+
+
+        void ChoosePlane()
+        {
+            int rando = Random.Range(0, mapPlanes.Length);
+            destination = mapPlanes[rando].transform.position;
+            agent.SetDestination(destination);
+        }
+    }
+
+}
