cfef86a18f99580600167d5a1da7951ba699dbea
jinzhihoi <56183521+jinzhihoi@users.noreply.github.com>
2021-02-24 17:33:27 +0800

    harpoon and flare minor changes
    xd

--- ProjectHadal/Assets/_PROJECT/Scripts/Inputs/StandardUseableInput.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Inputs/StandardUseableInput.cs
@@ -5,6 +5,7 @@ namespace Hadal.Inputs
     public class StandardUseableInput : IUseableInput
     {
         public bool FireKey1 => Input.GetMouseButton(0) || Input.GetMouseButtonDown(0);
-        public bool FireKey2 => Input.GetKeyDown(KeyCode.G);
+        public bool FireKey2 => Input.GetMouseButtonDown(1);
+        public bool FireKey2Held => Input.GetMouseButton(1);
     }
 }
--- ProjectHadal/Assets/_PROJECT/Scripts/Usables/Usable/Scriptable Scripts/HarpoonLauncherData.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Usables/Usable/Scriptable Scripts/HarpoonLauncherData.cs
@@ -9,12 +9,20 @@ namespace Hadal.Usables
     {
         public override void DoEffect(UsableHandlerInfo info)
         {
-            
+            var projectileObj = HarpoonPool.Instance.Scoop();
+            projectileObj.Data = ProjectileData;
+            projectileObj.DumpEvent += DumpProjectileMethod;
+            projectileObj.SetPositionRotation(info.FirePoint, info.Orientation);
+            projectileObj.WithGObjectSetActive(true);
+            if (projectileObj.PPhysics != null) projectileObj.PPhysics.LaunchProjectile();
         }
 
         protected override void DumpProjectileMethod(ProjectileBehaviour obj)
         {
-            
+            if (obj is HarpoonBehaviour harpoon)
+            {
+                HarpoonPool.Instance.Dump(harpoon);
+            }
         }
     }
 }
