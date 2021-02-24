984341f5376de0994b4cd86355934081ab51ec1a
Magthylius <magthylius@gmaillcom>
2021-02-24 19:26:25 +0800

    Update StandardUseableInput.cs

--- ProjectHadal/Assets/_PROJECT/Scripts/Inputs/StandardUseableInput.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Inputs/StandardUseableInput.cs
@@ -2,11 +2,20 @@
 
 namespace Hadal.Inputs
 {
+    //! E: Jon
     public class StandardUseableInput : IUseableInput
     {
-        public bool FireKey1 => Input.GetMouseButton(0) || Input.GetMouseButtonDown(0);
-        public bool FireKey2 => Input.GetMouseButtonDown(1);
-        public bool FireKey2Held => Input.GetMouseButton(1);
-        public bool FireKey2Release => Input.GetMouseButtonUp(1);
+        public bool FireKey1 => MB(0) || MBDown(0);
+        public bool FireKey2 => MBDown(1);
+        public bool FireKey2Held => MB(1);
+        public bool FireKey2Release => MBUp(1);
+        public bool EscKeyDown => KDown(KeyCode.Escape);
+        public bool EscKeyUp => KUp(KeyCode.Escape);
+
+        bool MB(int button) => Input.GetMouseButton(button);
+        bool MBDown(int button) => Input.GetMouseButtonDown(button);
+        bool MBUp(int button) => Input.GetMouseButtonUp(button);
+        bool KDown(KeyCode keyCode) => Input.GetKeyDown(keyCode);
+        bool KUp(KeyCode keyCode) => Input.GetKeyUp(keyCode);
     }
 }
