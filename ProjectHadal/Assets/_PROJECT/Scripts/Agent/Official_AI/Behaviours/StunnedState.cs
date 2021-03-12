4b8bfbe1355734d66162087c70dea5e7793b4d35
NicholasTiew <nicholastiewofficial@gmail.com>
2021-03-12 17:18:09 +0800

    Stunned State
    Stunned State

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/StunnedState.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/StunnedState.cs
@@ -2,15 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Tenshi.AIDolls;
+using Hadal.Utility;
+using Tenshi.UnitySoku;
 using System;
 
 namespace Hadal.AI
 {
     public class StunnedState : IState
     {
-        public void OnStateStart() { }
-        public void StateTick() { }
-        public void OnStateEnd() { }
-        public Func<bool> ShouldTerminate() => () => false;
+        public AIBrain Brain { get; private set; }
+        Timer stunTimer;
+        bool returnToDefaultState = false;
+
+        public StunnedState(AIBrain brain)
+        {
+            Brain = brain;
+            returnToDefaultState = false;
+            stunTimer = Brain.Create_A_Timer()
+                                .WithDuration(Brain.stunDuration)
+                                .WithOnCompleteEvent(() => returnToDefaultState = true)
+                                .WithOnUpdateEvent(_ => $"Stun timer: {(100f * stunTimer.GetCompletionRatio):F2}%".Msg())
+                                .WithShouldPersist(true);
+            Brain.AttachTimer(stunTimer);
+            stunTimer.Pause();
+        }
+        public void OnStateStart()
+        {
+            returnToDefaultState = false;
+            stunTimer.Restart();
+    
+            $"stunned".Msg();
+        }
+        public void StateTick()
+        {
+
+        }
+        public void OnStateEnd()
+        {
+            returnToDefaultState = false;
+            stunTimer.Pause();
+            $"no longer stunned".Msg();
+        }
+        public Func<bool> ShouldTerminate() => () => returnToDefaultState;
     }
 }
