48fd322ba2fe3a44a07e8ff990cf956c0a544355
NicholasTiew <nicholastiewofficial@gmail.com>
2021-03-12 21:47:17 +0800

    Push to Pull
    Push to Pull

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/AIHealthManager.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/AIHealthManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Tenshi;
+using Tenshi.UnitySoku;
 
 namespace Hadal.AI
 {
@@ -14,6 +15,15 @@ namespace Hadal.AI
         {
             if (maxHealth <= 0) maxHealth = 1;
             ResetHealth();
+        }
+
+        public void CheckHealth()
+        {
+            if (IsUnalive)
+            {
+                $"U.N. Owen has been unalived".Msg();
+                Obj.SetActive(false);
+            }
         }
 
         public bool TakeDamage(int damage)
