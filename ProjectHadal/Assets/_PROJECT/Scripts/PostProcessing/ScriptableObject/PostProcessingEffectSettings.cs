c9ce30e2b8b1953e3e4afaf74312c46f754c8b11
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-06-03 19:30:40 +0800

    Update PostProcessingEffectSettings.cs

--- ProjectHadal/Assets/_PROJECT/Scripts/PostProcessing/ScriptableObject/PostProcessingEffectSettings.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/PostProcessing/ScriptableObject/PostProcessingEffectSettings.cs
@@ -11,6 +11,40 @@ namespace Hadal.PostProcess.Settings
     }
 
     [System.Serializable]
+    public struct DepthOfFieldSettings
+    {
+        public DepthOfFieldMode Mode;
+        [Min(0.1f)] public float FocusDistance;
+        [Range(1f, 150f)] public float FocusLength;
+        [Range(1f, 32f)] public float Aperture;
+        [Range(3, 9)] public int BladeCount;
+        [Range(0f, 1f)] public float BladeCurvature;
+        [Range(-180f, 180f)] public float BladeRotation;
+
+        public DepthOfFieldSettings(in DepthOfField depthOfField)
+        {
+            Mode = depthOfField.mode.value;
+            FocusDistance = depthOfField.focusDistance.value;
+            FocusLength = depthOfField.focalLength.value;
+            Aperture = depthOfField.aperture.value;
+            BladeCount = depthOfField.bladeCount.value;
+            BladeCurvature = depthOfField.bladeCurvature.value;
+            BladeRotation = depthOfField.bladeRotation.value;
+        }
+
+        public DepthOfFieldSettings(DepthOfFieldSettings depthOfField)
+        {
+            Mode = depthOfField.Mode;
+            FocusDistance = depthOfField.FocusDistance;
+            FocusLength = depthOfField.FocusLength;
+            Aperture = depthOfField.Aperture;
+            BladeCount = depthOfField.BladeCount;
+            BladeCurvature = depthOfField.BladeCurvature;
+            BladeRotation = depthOfField.BladeRotation;
+        }
+    }
+
+    [System.Serializable]
     public struct LensDistortionSettings 
     {
         [Range(-1f, 1f)] public float Intensity;
