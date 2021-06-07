eab3bef1bcdf495c7d314c370c78ea6fd892a187
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-06-07 18:46:11 +0800

    working on tail whip node

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/BehaviorTree/Judgement Tree/TailWhipNode.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/BehaviorTree/Judgement Tree/TailWhipNode.cs
@@ -1,22 +1,33 @@
+using System.Collections;
 using Tenshi.UnitySoku;
+using UnityEngine;
 
 namespace Hadal.AI.TreeNodes
 {
     public class TailWhipNode : BTNode
     {
         private AIBrain _brain;
+        private Collider _tailCollider;
         private bool _tailWhipDone;
+        private bool _triggerOnce;
+        private float _whipTimer;
+        private float _whipTime;
 
         public TailWhipNode(AIBrain brain)
         {
             _brain = brain;
             _tailWhipDone = false;
+            _triggerOnce = false;
         }
 
         public override NodeState Evaluate()
         {
-            // trigger tailwhip code here
-            _tailWhipDone = true;
+            if (!_triggerOnce)
+            {
+                _triggerOnce = true;
+                // trigger tailwhip code here
+                _brain.StartCoroutine(WhipRoutine());
+            }
 
             if (!_tailWhipDone)
                 return NodeState.RUNNING;
@@ -24,5 +35,23 @@ namespace Hadal.AI.TreeNodes
             "Whips something?".Msg();
             return NodeState.SUCCESS;
         }
+
+        private IEnumerator WhipRoutine()
+        {
+            while (!_tailWhipDone)
+            {
+                if (TickWhipTimer(_brain.DeltaTime) <= 0)
+                {
+                    ResetWhipTimer();
+                    // disable collider here
+                    _tailWhipDone = true;
+                }
+                yield return null;
+            }
+            _triggerOnce = false;
+        }
+
+        private float TickWhipTimer(in float deltaTime) => _whipTimer -= deltaTime;
+        private void ResetWhipTimer() => _whipTimer = _whipTime;
     }
 }
