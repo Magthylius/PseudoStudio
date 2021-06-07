ee4e51fb0fcb9ebde388ee5a8160709c3b59d5af
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-06-08 00:23:19 +0800

    improved sense detection script

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Detection/AISenseDetection.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Detection/AISenseDetection.cs
@@ -9,55 +9,52 @@ namespace Hadal.AI
     public class AISenseDetection : MonoBehaviour, ILeviathanComponent
     {
         [SerializeField] float overlapSphereDetectionRadius;
-        [SerializeField] Vector3 detectPoint;
+        [SerializeField] Vector3 detectionOffset;
+        [SerializeField] float checkDelay;
         public int DetectedPlayersCount { get; private set; }
+        public List<PlayerController> DetectedPlayers => new List<PlayerController>(_detectedPlayers);
+        private float _checkTimer;
+        private List<PlayerController> _detectedPlayers;
         private AIBrain _brain;
-
 
         public UpdateMode LeviathanUpdateMode => UpdateMode.PreUpdate;
 
         public void Initialise(AIBrain brain)
         {
             _brain = brain;
+            _checkTimer = 0f;
+            _detectedPlayers = new List<PlayerController>();
+            DetectedPlayersCount = 0;
         }
 
         public void DoUpdate(in float deltaTime)
         {
-            SenseSurrondings();
-        }
-
-        public void DoFixedUpdate(in float fixedDeltaTime)
-        {
-        }
-
-        public void DoLateUpdate(in float deltaTime)
-        {
-        }
-
-        // void ConvertTransformToGO()
-        // {
-        //     //players = _brain.PlayerTransforms.Select(player => player.gameObject).ToArray();
-        // }
-
-        //! Another way of doing this is making a big collider as a child and detect through ontriggerenter
-        //! We probably gonna call this once the AI sees a player to detect its surrondings how many players are there
-        private void SenseSurrondings()
-        {
-            Collider[] playerSphere = Physics.OverlapSphere(transform.position + detectPoint, overlapSphereDetectionRadius, _brain.RuntimeData.PlayerMask);
-            if (_brain.DebugEnabled) Debug.Log("MY SPIDEY SENSE IS TINGLING");
-            DetectedPlayersCount = playerSphere.Length;
-            if (_brain.DebugEnabled) Debug.Log("I SENSE:" + DetectedPlayersCount);
-            
-            if (DetectedPlayersCount > 0)
+            if (TickTimer(deltaTime) <= 0)
             {
-                _brain.CurrentTarget = playerSphere.First().GetComponent<PlayerController>();
+                ResetTimer();
+                SenseSurrondings();
             }
         }
+
+        public void DoFixedUpdate(in float fixedDeltaTime) { }
+
+        public void DoLateUpdate(in float deltaTime) { }
+
+        private void SenseSurrondings()
+        {
+            Collider[] playerSphere = Physics.OverlapSphere(transform.position + detectionOffset, overlapSphereDetectionRadius, _brain.RuntimeData.PlayerMask);
+            DetectedPlayersCount = playerSphere.Length;
+            _detectedPlayers = playerSphere.Select(c => c.GetComponent<PlayerController>()).ToList();
+            if (_brain.DebugEnabled) Debug.Log("I SENSE:" + DetectedPlayersCount);
+        }
+
+        private float TickTimer(in float deltaTime) => _checkTimer -= deltaTime;
+        private void ResetTimer() => _checkTimer = checkDelay;
 
         void OnDrawGizmos()
         {
             Gizmos.color = Color.grey;
-            Gizmos.DrawWireSphere(transform.position + detectPoint, overlapSphereDetectionRadius);
+            Gizmos.DrawWireSphere(transform.position + detectionOffset, overlapSphereDetectionRadius);
         }
     }
 }
