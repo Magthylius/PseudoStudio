a14ea80836437ca04e432bd5cf24aff430679257
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-06-15 19:18:25 +0800

    Update AIPackageInfo.cs

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/AIPackageInfo.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/AIPackageInfo.cs
@@ -54,7 +54,7 @@ namespace Hadal.AI.Information
                 brain = GetComponentInChildren<AIBrain>();
                 navHandler = GetComponentInChildren<PointNavigationHandler>();
                 yield return null;
-            } while (navHandler == null || brain == null || brain.TargetMoveCavern == null);
+            } while (navHandler == null || brain == null);
             
             StartUpdate();
         }
@@ -64,12 +64,8 @@ namespace Hadal.AI.Information
             while (enableUpdate)
             {
                 //! AIBrain
-                targetCavern = brain.TargetMoveCavern.cavernTag;
-                
-                if (brain.CavernManager.GetHandlerOfAILocation)
-                    currentCavern = brain.CavernManager.GetHandlerOfAILocation.cavernTag;
-                else 
-                    currentCavern = CavernTag.Invalid;
+                targetCavern = brain.TargetMoveCavern ? brain.TargetMoveCavern.cavernTag : CavernTag.Invalid;
+                currentCavern = brain.CavernManager.GetHandlerOfAILocation ? brain.CavernManager.GetHandlerOfAILocation.cavernTag : CavernTag.Invalid;
                 
                 state = brain.GetState;
 
