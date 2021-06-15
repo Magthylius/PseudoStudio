215d42bc1b533b19f72ba458348b4bffee212e81
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-06-15 21:14:09 +0800

    Added AI graphics for reference handling

---
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Graphics/AIGraphicsHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hadal.AI.Graphics
+{
+    public class AIGraphicsHandler : MonoBehaviour
+    {
+        public GameObject MouthObject;
+    }
+}
