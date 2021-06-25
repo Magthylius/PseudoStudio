aeb777c9644122a89dc210a2933c133837928b9c
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-06-25 16:31:59 +0800

    Mushroom behaviour group

--- ProjectHadal/Assets/_PROJECT/Scripts/Interactable/MushroomBehaviour.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Interactable/MushroomBehaviour.cs
@@ -8,167 +8,62 @@ namespace Hadal.Interactables
     [RequireComponent(typeof(Collider))]
     public class MushroomBehaviour : MonoBehaviour
     {
-        [Header("Settings")]
-        [SerializeField, MinMaxSlider(0.1f, 10f)] private Vector2 reactiveTimeoutRange;
-
-        [Header("Colour Data")]
-        [SerializeField] private MushroomShaderData reactiveData;
-        [SerializeField, ReadOnly] private MushroomShaderData unreactiveData;
-        [SerializeField, ReadOnly] private MushroomShaderData currentData;
-        [SerializeField, ReadOnly] private float _percent;
-
         [Header("Renderers")]
-        [SerializeField] private List<MeshRenderer> renderersInner;
-        [SerializeField] private List<MeshRenderer> renderersOuter;
+        [SerializeField] private MeshRenderer renderersInner;
+        [SerializeField] private MeshRenderer renderersOuter;
 
-        [Header("Misc")]
-        [SerializeField] private float reactiveTransitionSpeed;
-        [SerializeField] private float unreactiveTransitionSpeed;
-        [SerializeField] private LayerMask reactiveMasks;
-        [SerializeField, ReadOnly] private List<Material> materialsInner;
-        [SerializeField, ReadOnly] private List<Material> materialsOuter;
+        private MaterialPropertyBlock outerBlock;
+        private MaterialPropertyBlock innerBlock;
 
-        private bool _canBecomeUnreactive;
-        private bool _isReactive;
-        private float reactiveTimeoutTimer;
-        private HashSet<Collider> contacts;
+        public MeshRenderer GetInnerRenderer => renderersInner;
+        public MeshRenderer GetOuterRenderer => renderersOuter;
 
+        public void Initialise()
+        {
+            innerBlock = new MaterialPropertyBlock();
+            outerBlock = new MaterialPropertyBlock();
+            if (GetInnerRenderer != null) GetInnerRenderer.GetPropertyBlock(innerBlock);
+            if (GetOuterRenderer != null) GetOuterRenderer.GetPropertyBlock(outerBlock);
+        }
+
+        public void SetMaterialEmissionData(float rate, float breathing)
+        {
+            innerBlock.SetFloat(EmissionRate, rate);
+            innerBlock.SetFloat(EmissionBreathing, breathing);
+            SetInnerBlock();
+        }
+
+        public void SetMaterialData(MushroomShaderData data)
+        {
+            outerBlock.SetColor(BorderColour, data.BorderColour);
+            outerBlock.SetFloat(BorderPower, data.BorderPower);
+            outerBlock.SetFloat(NoiseScale, data.NoiseScale);
+            outerBlock.SetFloat(Alpha, data.Alpha);
+            SetOuterBlock();
+
+            innerBlock.SetColor(Colour, data.Colour);
+            SetInnerBlock();
+        }
+
+        private void SetInnerBlock()
+        {
+            GetInnerRenderer.SetPropertyBlock(innerBlock);
+        }
+
+        private void SetOuterBlock()
+        {
+            GetOuterRenderer.SetPropertyBlock(outerBlock);
+        }
+
+        #region Macro Strings
         private readonly string BorderColour = "_BorderColor";
         private readonly string Colour = "_Color";
         private readonly string BorderPower = "_BorderPower";
         private readonly string NoiseScale = "_NoiseScale";
-		private readonly string Alpha = "_Alpha";
-		private readonly string EmissionRate = "_EmissionRate";
-		private readonly string EmissionBreathing = "_EmissionBreathing";
-
-        private void Awake()
-        {
-            renderersInner ??= new List<MeshRenderer>();
-            renderersOuter ??= new List<MeshRenderer>();
-            materialsInner ??= new List<Material>();
-            materialsOuter ??= new List<Material>();
-            _canBecomeUnreactive = false;
-            _isReactive = false;
-            contacts = new HashSet<Collider>();
-            _percent = 0f;
-
-            currentData = new MushroomShaderData();
-            int count = renderersOuter.Count;
-            int i = -1;
-            while (++i < count)
-            {
-                materialsOuter.Add(renderersOuter[i].material);
-                materialsOuter[i].EnableKeyword("_ALPHAPREMULTIPLY_ON");
-                materialsOuter[i].SetFloat(BorderPower, 0.3f);
-
-                materialsInner.Add(renderersInner[i].material);
-                materialsInner[i].EnableKeyword("_ALPHAPREMULTIPLY_ON");
-            }
-
-            //! Default colour data
-            unreactiveData = new MushroomShaderData
-            {
-                BorderColour = materialsOuter[0].GetColor(BorderColour),
-                Colour = materialsInner[0].GetColor(Colour),
-                BorderPower = materialsOuter[0].GetFloat(BorderPower),
-                NoiseScale = materialsOuter[0].GetFloat(NoiseScale),
-				Alpha = materialsOuter[0].GetFloat(Alpha)
-            };
-			
-			//! Default emission settings
-			materialsInner[0].SetFloat(EmissionRate, Random.Range(reactiveData.EmissionRate.x, reactiveData.EmissionRate.y));
-			materialsInner[0].SetFloat(EmissionBreathing, Random.Range(reactiveData.EmissionBreathing.x, reactiveData.EmissionBreathing.y));
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (!HasViableColourData || !CanTransition) return;
-            if (CanCollide(other))
-            {
-                if (contacts.Contains(other))
-                    return;
-                contacts.Add(other);
-
-                if (_isReactive)
-                    return;
-                _canBecomeUnreactive = false;
-                StopAllCoroutines();
-                StartCoroutine(ReactiveTransition());
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (!HasViableColourData || !CanTransition) return;
-            if (CanCollide(other))
-            {
-                contacts.Remove(other);
-                if (contacts.IsEmpty())
-                    _canBecomeUnreactive = true;
-            }
-        }
-
-        private void SetMaterialColour()
-        {
-            _percent = _percent.Clamp01();
-            currentData.Lerp(unreactiveData, reactiveData, _percent);
-
-            int count = materialsOuter.Count;
-            int i = -1;
-            while (++i < count)
-            {
-                materialsOuter[i].SetColor(BorderColour, currentData.BorderColour);
-                materialsInner[i].SetColor(Colour, currentData.Colour);
-                materialsOuter[i].SetFloat(BorderPower, currentData.BorderPower);
-                materialsOuter[i].SetFloat(NoiseScale, currentData.NoiseScale);
-				materialsOuter[i].SetFloat(Alpha, currentData.Alpha);
-            }
-        }
-
-        private IEnumerator ReactiveTransition()
-        {
-            _isReactive = true;
-
-            //! Become Reactive
-            while (_percent < 1f)
-            {
-                _percent += DeltaTime * reactiveTransitionSpeed;
-                SetMaterialColour();
-                yield return null;
-            }
-
-            //! Wait for unreactive cue & timer
-            while (!_canBecomeUnreactive)
-                yield return null;
-
-            yield return new WaitForSeconds(Random.Range(reactiveTimeoutRange.x, reactiveTimeoutRange.y));
-
-            //! Become Unreactive
-            StartCoroutine(UnreactiveTransition());
-        }
-
-        private IEnumerator UnreactiveTransition()
-        {
-            while (_percent > 0f)
-            {
-                _percent -= DeltaTime * unreactiveTransitionSpeed;
-                SetMaterialColour();
-                yield return null;
-            }
-
-            _isReactive = false;
-        }
-
-        private float DeltaTime => Time.deltaTime;
-        private bool CanCollide(Collider other) => other.gameObject.layer.IsAMatchingMask(reactiveMasks);
-        private bool CanTransition
-            => !renderersOuter.IsNullOrEmpty()
-            && !materialsOuter.IsNullOrEmpty();
-        private bool HasViableColourData => unreactiveData != null && reactiveData != null;
-
-        private void ResetTimeoutTimer() => reactiveTimeoutTimer = Random.Range(reactiveTimeoutRange.x, reactiveTimeoutRange.y);
-        private float TickTimeoutTimer(in float deltaTime) => reactiveTimeoutTimer -= deltaTime;
-        private bool ReactiveTimeoutTimerReached => reactiveTimeoutTimer <= 0f;
+        private readonly string Alpha = "_Alpha";
+        private readonly string EmissionRate = "_EmissionRate";
+        private readonly string EmissionBreathing = "_EmissionBreathing";
+        #endregion
     }
 
     [System.Serializable]
@@ -178,16 +73,16 @@ namespace Hadal.Interactables
         public Color Colour = Color.white;
         public float BorderPower = 0f;
         public float NoiseScale = 0f;
-		public float Alpha = 0.9f;
-		[MinMaxSlider(0.1f, 50f)] public Vector2 EmissionRate;
-		[MinMaxSlider(0.1f, 50f)] public Vector2 EmissionBreathing;
+        public float Alpha = 0.9f;
+        [MinMaxSlider(0.1f, 50f)] public Vector2 EmissionRate;
+        [MinMaxSlider(0.1f, 50f)] public Vector2 EmissionBreathing;
         public void Lerp(MushroomShaderData a, MushroomShaderData b, in float percent)
         {
             BorderColour = Color.Lerp(a.BorderColour, b.BorderColour, percent);
             Colour = Color.Lerp(a.Colour, b.Colour, percent);
             BorderPower = Mathf.Lerp(a.BorderPower, b.BorderPower, percent);
             NoiseScale = Mathf.Lerp(a.NoiseScale, b.NoiseScale, percent);
-			Alpha = Mathf.Lerp(a.Alpha, b.Alpha, percent);
+            Alpha = Mathf.Lerp(a.Alpha, b.Alpha, percent);
         }
     }
 }
---
+++ ProjectHadal/Assets/_PROJECT/Scripts/Interactable/MushroomBehaviourGroup.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Tenshi;
+using UnityEngine;
+
+namespace Hadal.Interactables
+{
+    public class MushroomBehaviourGroup : MonoBehaviour
+    {
+        [Header("Mushrooms")]
+        [SerializeField] private string groupName = string.Empty;
+        [SerializeField] private List<MushroomBehaviour> mushrooms;
+
+        [Header("Internal Data")]
+        [SerializeField] private MushroomShaderData reactiveData;
+        [SerializeField, ReadOnly] private MushroomShaderData defaultData;
+        [SerializeField, ReadOnly] private MushroomShaderData currentData;
+        [SerializeField, ReadOnly] private float percent;
+        private bool _canBecomeUnreactive;
+        private bool _isReactive;
+        private HashSet<Collider> contacts;
+
+        [Header("Settings")]
+        [SerializeField, MinMaxSlider(0.1f, 60f)] private Vector2 reactiveTimeoutRange;
+        [SerializeField] private LayerMask reactiveMask;
+        [SerializeField] private float reactiveTransitionSpeed;
+        [SerializeField] private float unreactiveTransitionSpeed;
+        
+        private void Awake()
+        {
+            contacts = new HashSet<Collider>();
+            if (groupName != string.Empty)
+            {
+                gameObject.name += $" - {groupName}";
+            }
+            if (mushrooms.IsNullOrEmpty())
+            {
+                mushrooms = new List<MushroomBehaviour>();
+                return;
+            }
+
+            int i = -1;
+            while (++i < mushrooms.Count)
+                mushrooms[i].Initialise();
+            
+            //! Caching default material/shader data
+            MushroomBehaviour mush = mushrooms.FirstOrDefault();
+            if (mush == null) return;
+            var rendOuter = mush.GetOuterRenderer;
+            var rendInner = mush.GetInnerRenderer;
+            defaultData = new MushroomShaderData
+            {
+                BorderColour = rendOuter.sharedMaterial.GetColor(BorderColour),
+                Colour = rendInner.sharedMaterial.GetColor(Colour),
+                BorderPower = rendOuter.sharedMaterial.GetFloat(BorderPower),
+                NoiseScale = rendOuter.sharedMaterial.GetFloat(NoiseScale),
+                Alpha = rendOuter.sharedMaterial.GetFloat(Alpha)
+            };
+
+            //! Randomised starting emission settings
+            float rate = Random.Range(reactiveData.EmissionRate.x, reactiveData.EmissionRate.y);
+            float breathing = Random.Range(reactiveData.EmissionBreathing.x, reactiveData.EmissionBreathing.y);
+            i = -1;
+            while (++i < mushrooms.Count)
+                mushrooms[i].SetMaterialEmissionData(rate, breathing);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!CanCollide(other) || contacts.Contains(other)) return;
+            contacts.Add(other);
+
+            if (_isReactive)
+                return;
+            _canBecomeUnreactive = false;
+            StopAllCoroutines();
+            StartCoroutine(ReactiveTransition());
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!CanCollide(other)) return;
+            contacts.Remove(other);
+            
+            if (contacts.Count == 0)
+                _canBecomeUnreactive = true;
+        }
+
+        private void UpdateShaderData()
+        {
+            percent = percent.Clamp01();
+            currentData.Lerp(defaultData, reactiveData, percent);
+
+            int count = mushrooms.Count;
+            int i = -1;
+            while (++i < count)
+                mushrooms[i].SetMaterialData(currentData);
+        }
+
+        private IEnumerator ReactiveTransition()
+        {
+            _isReactive = true;
+
+            //! Reactive lerp
+            while (percent < 1f)
+            {
+                percent += DeltaTime * reactiveTransitionSpeed;
+                UpdateShaderData();
+                yield return null;
+            }
+
+            //! Wait for unreactive cue & timer
+            WaitForSeconds waitTime = new WaitForSeconds(1f);
+            while (!_canBecomeUnreactive)
+                yield return waitTime;
+
+            //! Persisting reactivity timeout
+            yield return new WaitForSeconds(Random.Range(reactiveTimeoutRange.x, reactiveTimeoutRange.y));
+
+            StartCoroutine(UnreactiveTransition());
+        }
+        private IEnumerator UnreactiveTransition()
+        {
+            //! Unreactive lerp
+            while (percent > 0f)
+            {
+                percent -= DeltaTime * unreactiveTransitionSpeed;
+                UpdateShaderData();
+                yield return null;
+            }
+            
+            _isReactive = false;
+        }
+
+        private float DeltaTime => Time.deltaTime;
+        private bool CanCollide(Collider other) => other.gameObject.layer.IsAMatchingMask(reactiveMask);
+
+        #region Macro Strings
+        private readonly string BorderColour = "_BorderColor";
+        private readonly string Colour = "_Color";
+        private readonly string BorderPower = "_BorderPower";
+        private readonly string NoiseScale = "_NoiseScale";
+        private readonly string Alpha = "_Alpha";
+        private readonly string EmissionRate = "_EmissionRate";
+        private readonly string EmissionBreathing = "_EmissionBreathing";
+        #endregion
+    }
+}
