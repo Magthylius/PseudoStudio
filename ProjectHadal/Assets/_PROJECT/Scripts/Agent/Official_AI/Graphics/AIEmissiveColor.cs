71714e90f35885ffcd2c64a8182c6a7f3ff939b7
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-07-02 13:54:29 +0800

    Changed aiemissive colour script to use property blocks

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Graphics/AIEmissiveColor.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Graphics/AIEmissiveColor.cs
@@ -8,6 +8,7 @@ namespace Hadal.AI.Graphics
     {
         [Header("Settings")]
         [SerializeField] private Material leviathanBody;
+        [SerializeField] private MeshRenderer leviathanRenderer;
         
         [SerializeField] private float transitionSpeed;
         private float percent;
@@ -19,35 +20,68 @@ namespace Hadal.AI.Graphics
         [ColorUsageAttribute(true, true)]
         public Color judgementStateColor;
 
+        /// <summary> Having a cached reference can help save computation cost. </summary>
+        private MaterialPropertyBlock materialProp;
+
         void Start()
         {
             brain = FindObjectOfType<AIBrain>();
             brain.JudgementPhaseEvent += JudgementColor;
+
+            //! initialise property block (needs to use a renderer, since it has the functions to set up)
+            materialProp = new MaterialPropertyBlock();
+            if (materialProp != null) leviathanRenderer.GetPropertyBlock(materialProp);
         }
         public void JudgementColor(bool isStarting)
         {
-            if(isStarting)
+            StopAllCoroutines(); //! stopping any running coroutines so it will never run more than once per event call
+            StartCoroutine(AIColorLerp(isStarting));
+        }
+
+        /// <summary>
+        /// Lerps the material colour of the leviathan renderer to change its colour. Percent will up go towards 1f if isJudgement is true;
+        /// percent will up go down towards 0f if isJudgement is false.
+        /// </summary>
+        IEnumerator AIColorLerp(bool isJudgement)
+        {
+            if (isJudgement)
             {
-                StartCoroutine(AIColorLerp(judgementStateColor));
                 //Debug.Log("judgementstartcolor");
+                while (percent < 1f)
+                {
+                    percent += brain.DeltaTime * transitionSpeed;
+                    UpdateMaterialData();
+                    yield return null;
+                }
+                percent = 1f;
             }
             else
             {
-                StartCoroutine(AIColorLerp(anticipationStateColor));
                 //Debug.Log("judgementendcolor");
+                while (percent > 0f)
+                {
+                    percent -= brain.DeltaTime * transitionSpeed;
+                    UpdateMaterialData();
+                    yield return null;
+                }
+                percent = 0f;
             }
-                
+
+            // while (percent < 1f)
+            // {
+            //     percent += Time.deltaTime * transitionSpeed;
+            //     leviathanBody.SetColor("_EmissionColor", Color.Lerp(leviathanBody.GetColor("_EmissionColor"), targetColor, percent));
+            //     yield return null;
+            // }
         }
 
-        IEnumerator AIColorLerp(Color targetColor)
+        private void UpdateMaterialData()
         {
-            while (percent < 1f)
-            {
-                percent += Time.deltaTime * transitionSpeed;
-                leviathanBody.SetColor("_EmissionColor", Color.Lerp(leviathanBody.GetColor("_EmissionColor"), targetColor, percent));
-                yield return null;
-            }
+            materialProp.SetColor(LerpColourValue(percent));
+            leviathanRenderer.SetPropertyBlock(materialProp);
         }
+
+        private Color LerpColourValue(in float percent) => Color.Lerp(anticipationStateColor, judgementStateColor, percent);
 
         void OnDestroy()
         {
