68c1097ab2ec92d63f430d8cbb4f26141782ec6b
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-03 03:59:23 +0800

    removed debugs

--- ProjectHadal/Assets/_PROJECT/Scripts/UsablePlayerBridge/UITrackerBridge.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/UsablePlayerBridge/UITrackerBridge.cs
@@ -14,7 +14,7 @@ public static class UITrackerBridge
 
     public static void Reset()
     {
-        Debug.LogWarning("what?");
+        //Debug.LogWarning("what?");
         LocalPlayerUIManager = null;
         OtherPlayerNames = new Dictionary<Transform, string>();
         PlayerAddedEvent = null;
