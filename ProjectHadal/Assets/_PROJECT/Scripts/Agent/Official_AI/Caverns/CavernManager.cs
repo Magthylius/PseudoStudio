e6fa3177438041623d21d7845a4102a8a12a44da
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-07-02 23:41:13 +0800

    blood asset injured

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Caverns/CavernManager.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Caverns/CavernManager.cs
@@ -145,6 +145,7 @@ namespace Hadal.AI.Caverns
                 GetHandlerOfAILocation = null;
 
             AILeftCavernEvent?.Invoke(handler);
+            Debug.Break();
         }
 
         public void OnPlayerEnterTunnel(TunnelPlayerData data)
