f4b7746b3848e8c86e219e158f3609e2f60b3a03
Harry <59987047+HarryKing-Hub@users.noreply.github.com>
2021-07-02 23:57:39 +0800

    explosion wip

--- ProjectHadal/Assets/_PROJECT/Scripts/Interactable/ExplosionVFX.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Interactable/ExplosionVFX.cs
@@ -12,19 +12,30 @@ namespace Hadal.Interactables
         [SerializeField] private Transform explosionAura;
         [SerializeField] private Transform explosionAura2;
 
+        [SerializeField] private float startScale;
+        [SerializeField] private float endScale;
+        [SerializeField] private float speed;
+
         // Start is called before the first frame update
         void Start()
         {
             
         }
+        
         void OnEnable()
         {
+            //explosionAura.transform.localScale = new Vector3(0,0,0);
+            //explosionAura2.transform.localScale = new Vector3(0,0,0);
             StartCoroutine(ExplosionActivate());
         }
 
         IEnumerator ExplosionActivate()
         {
-            yield return null;
+            for(float t = 1.0f; t >= -1.1f; t-= Time.deltaTime * speed)
+            {
+                explosion.SetFloat("_Addition", t);
+                yield return null;
+            }
         }
     }
 }
