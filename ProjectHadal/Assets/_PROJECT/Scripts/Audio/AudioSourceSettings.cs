3a8133bf7e66ca3d89a7827436468a3ada09b228
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-07-06 16:38:44 +0800

    made volume varied

--- ProjectHadal/Assets/_PROJECT/Scripts/Audio/AudioSourceSettings.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Audio/AudioSourceSettings.cs
@@ -7,14 +7,14 @@ namespace Hadal.AudioSystem
     [System.Serializable]
     public class AudioSourceSettings
     {
-        [SerializeField, Range(0f, 1f)] private float Volume;
-        [SerializeField, MinMaxSlider(.1f, 3f)] private Vector2 Pitch;
-        [SerializeField, Range(0f, 1f)] private float SpatialBlend;
+        [SerializeField, MinMaxSlider(0f, 1f)] private Vector2 Volume = Vector2.one;
+        [SerializeField, MinMaxSlider(.1f, 3f)] private Vector2 Pitch = Vector2.one;
+        [SerializeField, Range(0f, 1f)] private float SpatialBlend = 0.7f;
         [SerializeField] private AudioMixerGroup MixerGroup;
 
         public void AssignSettings(AudioSource source)
         {
-            source.volume = Volume;
+            source.volume = Volume.RandomBetweenXY();
             source.pitch = Pitch.RandomBetweenXY();
             source.spatialBlend = SpatialBlend;
             source.outputAudioMixerGroup = MixerGroup;
