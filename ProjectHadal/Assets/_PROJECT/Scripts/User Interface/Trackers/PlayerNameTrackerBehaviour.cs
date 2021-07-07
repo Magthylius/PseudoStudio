fd62c80d26a3c1f317f5109a06fd46d97cb96d66
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-07 18:52:14 +0800

    fixed loading manager and added dsitance fader on name tag

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/Trackers/PlayerNameTrackerBehaviour.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/Trackers/PlayerNameTrackerBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,31 @@ namespace Hadal.UI
 {
     public class PlayerNameTrackerBehaviour : UITrackerBehaviour
     {
+        [Header("Name Tracker Settings")]
         public TextMeshProUGUI NameText;
-        
+        public TextMeshProUGUI DistanceText;
+
+        public float DistanceUpdateDelay = 1f;
+
+        private void Start()
+        {
+            base.Start();
+            InvokeRepeating(nameof(DistanceUpdater), 0f, DistanceUpdateDelay);
+        }
+
+        void DistanceUpdater()
+        {
+            UpdateDistance(distanceToTransform);
+        }
+
         public void UpdateText(string name)
         {
             NameText.text = name;
         }
 
+        public void UpdateDistance(float distance)
+        {
+            DistanceText.text = $"{(int)distance + " m"}";
+        }
     }
 }
