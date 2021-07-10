c06373cece21b489741747a6f076830fdb4a2a9a
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-10 17:43:48 +0800

    uishoot tracer updated

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIShootTracer.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIShootTracer.cs
@@ -8,37 +8,61 @@ namespace Hadal.UI
     public class UIShootTracer : MonoBehaviour
     {
         public LineRenderer line;
+        public Light hitLight;
         public Transform lineStartTransform;
-        private Vector3 lineEndPoint;
 
+        private Transform playerCamera;
+        
         private bool isActive = false;
+        private RaycastHit forwardHit;
+
+        private LayerMask rayIgnoreMask;
 
         private void Start()
         {
             //Deactivate();
             Vector3 startPos = lineStartTransform.position;
             line.SetPositions(new [] {startPos, startPos});
+            hitLight.gameObject.SetActive(false);
         }
 
-        private void FixedUpdate()
+        private void LateUpdate()
         {
+            if (playerCamera == null)
+            {
+                Debug.LogWarning("Player camera is null on shoot tracer!");
+                return;
+            }
+
+            Physics.Raycast(playerCamera.position, playerCamera.forward, out forwardHit,
+                Mathf.Infinity, ~rayIgnoreMask, QueryTriggerInteraction.Ignore);
+
             if (!isActive) return;
-            line.SetPositions(new [] {lineStartTransform.position, lineEndPoint});
+            line.SetPositions(new [] {lineStartTransform.position, forwardHit.point});
+            hitLight.transform.position = forwardHit.point;
         }
 
-        public void SetStartPoint(Transform startTransform) => lineStartTransform = startTransform;
-        public void SetEndPoint(Vector3 endWorldPoint) => lineEndPoint = endWorldPoint;
+        public void InjectDependencies(Camera pCamera) => playerCamera = pCamera.transform;
 
-        public void Activate() => isActive = true;
+        public void Activate()
+        {
+            isActive = true;
+            hitLight.gameObject.SetActive(true);
+        }
 
         public void Deactivate()
         {
             isActive = false;
             //! make it hide itself
             line.SetPosition(1, lineStartTransform.position);
+            
+            hitLight.gameObject.SetActive(false);
+            hitLight.transform.position = lineStartTransform.position;
         }
 
         public bool IsActive => isActive;
+
+        public Vector3 HitPoint => forwardHit.point;
     }
 
 }
