f7bb28ab2c1bd7ff19753c1e5b0bff5c30b2ba79
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-14 16:57:07 +0800

    added jumpstart UI

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIContextHandler.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIContextHandler.cs
@@ -7,6 +7,12 @@ namespace Hadal.UI
 {
     public class UIContextHandler : MonoBehaviour
     {
+        private void Start()
+        {
+            InitReviveUI();
+        }
+
+        
         [Header("Revive UI")] 
         [SerializeField] private Animator reviveAnimator;
         [SerializeField] private GameObject reviveDown;
@@ -15,7 +21,7 @@ namespace Hadal.UI
         private static readonly int PlayerDownHash = Animator.StringToHash("PlayerDown");
         private static readonly int PlayerRevivedHash = Animator.StringToHash("PlayerRevived");
 
-        private void Start()
+        void InitReviveUI()
         {
             reviveDown.SetActive(false);
             reviveUp.SetActive(false);
@@ -30,10 +36,18 @@ namespace Hadal.UI
         
         public void PlayerRevived()
         {
-            print("revived!");
+            //print("revived!");
             reviveAnimator.SetTrigger(PlayerRevivedHash);
-            //reviveDown.SetActive(false);
-            //reviveUp.SetActive(true);
         }
+        
+        [Header("Jumpstart UI")]
+        [SerializeField] private Animator jumpstartAnimator;
+        private static readonly int AllowJumpstart = Animator.StringToHash("AllowJumpstart");
+        private static readonly int JumpstartSucceed = Animator.StringToHash("JumpstartSucceed");
+        private static readonly int JumpstartFailure = Animator.StringToHash("JumpstartFailure");
+
+        public void StartJumpstart() => jumpstartAnimator.SetTrigger(AllowJumpstart);
+        public void SuccessJumpstart() => jumpstartAnimator.SetTrigger(JumpstartSucceed);
+        public void FailJumpstart() => jumpstartAnimator.SetTrigger(JumpstartFailure);
     }
 }
