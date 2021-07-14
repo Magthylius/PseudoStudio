914324fc424f99916fd1a3bb4f721618c3c6a34f
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-14 17:35:29 +0800

    Update PlayerHealthManager.cs

--- ProjectHadal/Assets/_PROJECT/Scripts/Player/Behaviours/PlayerHealthManager.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Player/Behaviours/PlayerHealthManager.cs
@@ -380,7 +380,7 @@ namespace Hadal.Player.Behaviours
             _shouldRevive = false;
             TryRestoreControllerSystem();
             CheckHealthStatus();
-			OnReviveAttempt?.Invoke(true);
+			//OnReviveAttempt?.Invoke(true);
             Send_HealthUpdateStatus(false); //! send revive message to non-local players
         }
 
