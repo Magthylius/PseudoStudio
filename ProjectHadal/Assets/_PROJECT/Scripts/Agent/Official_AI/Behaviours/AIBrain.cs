7c9d271abc1c40405a3f75265d2b1cd71d562748
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-07-22 17:56:28 +0800

    made the range of grab explode larger for testing

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/AIBrain.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/Official_AI/Behaviours/AIBrain.cs
@@ -547,7 +547,7 @@ namespace Hadal.AI
         {
             ExplosivePoint.ExplosionSettings expSettings = new ExplosivePoint.ExplosionSettings();
             expSettings.Position = position;
-            expSettings.Radius = SenseDetection.GetCurrentSenseDetectionRadius();
+            expSettings.Radius = SenseDetection.GetCurrentSenseDetectionRadius() + 100f;
             expSettings.Force = 20.0f;
             expSettings.IgnoreLayers = MachineData.Engagement.JG_KnockbackIgnoreMasks;
 
