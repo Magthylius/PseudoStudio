70b3c60684afee86103c78d8a96b3959ca22ead5
Hayaka-Sae <59986979+shijie-l@users.noreply.github.com>
2021-07-24 01:17:20 +0800

    updated ai

--- ProjectHadal/Assets/_PROJECT/Scripts/Agent/ScriptableObjects/AnticipationStateSettings.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Agent/ScriptableObjects/AnticipationStateSettings.cs
@@ -15,7 +15,7 @@ namespace Hadal.AI.States
 
         [Header("General")]
         [Range(0f, 1f)] public float ConfidenceObjectiveGate = 0.5f;
-        [MinMaxSlider(0f, 0.5f)] public Vector2 RandomConfidenceInfluence = Vector2.zero;
+        [MinMaxSlider(0f, 0.99f)] public Vector2 RandomConfidenceInfluence = Vector2.zero;
         [Min(0)] public int DisruptionDamageCount = 2;
         [Min(0)] public float IsolatedPlayerCheckTime = 5f;
 
