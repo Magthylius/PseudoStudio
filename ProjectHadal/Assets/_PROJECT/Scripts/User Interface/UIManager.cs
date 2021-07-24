da9e0d59c09bff77baaba18920a1c526f7452131
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-24 16:01:16 +0800

    hide UI button

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIManager.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIManager.cs
@@ -37,7 +37,7 @@ namespace Hadal.UI
         public UICockpitCamera CockpitCamera;
         public UIClassInfoHandler ClassInfoHandler;
         public Camera PlayerCamera;
-
+        
         [Header("Reticle Settings")]
         [SerializeField] RectTransform reticleGroup;
         [SerializeField] RectTransform reticleDirectors;
@@ -495,6 +495,27 @@ namespace Hadal.UI
 
         public bool IsOpen => pauseMenuOpen;
         public static bool IsNull => Instance == null;
+        #endregion
+
+        #region Debug
+
+        [Header("Hide All UI")] 
+        public List<GameObject> thingsToHide;
+
+        [Button("Hide All UI")]
+        public void HideUI()
+        {
+            if (gameObject != null)
+                foreach (GameObject go in thingsToHide) go.SetActive(false);
+        }
+
+        [Button("Show All UI")]
+        public void UnhideUI()
+        {
+            if (gameObject != null)
+                foreach (GameObject go in thingsToHide) go.SetActive(true);
+        }
+
         #endregion
     }
 }
