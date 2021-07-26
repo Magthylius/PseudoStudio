58865b6caf994adb6c117a95f1d268b6ee64bbf7
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-26 18:55:20 +0800

    iconography and select effects

--- ProjectHadal/Assets/_PROJECT/Scripts/Networking/UI/MainMenuHighlightBehaviour.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/Networking/UI/MainMenuHighlightBehaviour.cs
@@ -11,7 +11,10 @@ namespace Hadal.Networking.UI
 {
     public class MainMenuHighlightBehaviour : MonoBehaviour
     {
+        //public RectTransform iconRect;
+        //public float targetScale = 1.2f;
         public Image image;
+        public Image IconHighlight;
         public PlayerClassType ClassType;
 
         [SerializeField, ReadOnly] private bool selectedByOthers;
@@ -21,6 +24,21 @@ namespace Hadal.Networking.UI
             selectedByOthers = chosenByOthers;
             image.enabled = true;
             image.color = setColor;
+            //IconHighlight.color = setColor;
+
+            /*StartCoroutine(Enlarge());
+
+            IEnumerator Enlarge()
+            {
+                while (Vector3.SqrMagnitude(iconRect.localScale - TargetRectScale) > 0.1f)
+                {
+                    iconRect.localScale = Vector3.Lerp(iconRect.localScale, TargetRectScale, 5f * Time.deltaTime);
+                    Debug.LogWarning("??");
+                    yield return new WaitForEndOfFrame();
+                }
+
+                iconRect.localScale = TargetRectScale;
+            }*/
         }
 
         public void Deselect()
@@ -28,8 +46,23 @@ namespace Hadal.Networking.UI
             image.enabled = false;
             selectedByOthers = false;
             image.color = Color.white;
+
+            /*StartCoroutine(Shrink());
+            
+            IEnumerator Shrink()
+            { 
+                while (Vector3.SqrMagnitude(Vector3.one - TargetRectScale) > 0.1f)
+                {
+                    iconRect.localScale = Vector3.Lerp(iconRect.localScale, Vector3.one, 5f * Time.deltaTime);
+                    yield return new WaitForEndOfFrame();
+                }
+
+                iconRect.localScale = Vector3.one;
+            }*/
+            
         }
 
         public bool IsSelectedByOthers => selectedByOthers;
+        //private Vector3 TargetRectScale => new Vector3(targetScale, targetScale, targetScale);
     }
 }
