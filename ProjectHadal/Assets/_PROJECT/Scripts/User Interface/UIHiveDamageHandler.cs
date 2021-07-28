a4a45248b0eaa33f3d8517c25bea91c7ce8653c3
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-28 21:57:04 +0800

    tutorial fix damage hive

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIHiveDamageHandler.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIHiveDamageHandler.cs
@@ -9,12 +9,14 @@ namespace Hadal.UI
 {
     public class UIHiveDamageHandler : MonoBehaviour
     {
-        [SerializeField, ReadOnly] private List<Image> hives;
-
+        [SerializeField, ReadOnly] private List<Animator> hives;
+        
+        private static readonly int Flash = Animator.StringToHash("Flash");
+        
         [Button("Get all children as hives")]
         public void GetAllChildrenAsHives()
         {
-            hives = GetComponentsInChildren<Image>().ToList();
+            hives = GetComponentsInChildren<Animator>().ToList();
         }
 
         [Button("Random Hive Popup")]
@@ -26,15 +28,15 @@ namespace Hadal.UI
             int r4 = Random.Range(0, hives.Count);
             int r5 = Random.Range(0, hives.Count);
             
-            hives[r1].gameObject.SetActive(true);
-            hives[r2].gameObject.SetActive(true);
-            hives[r3].gameObject.SetActive(true);
-            hives[r4].gameObject.SetActive(true);
-            hives[r5].gameObject.SetActive(true);
+            hives[r1].SetTrigger(Flash);
+            hives[r2].SetTrigger(Flash);
+            hives[r3].SetTrigger(Flash);
+            hives[r4].SetTrigger(Flash);
+            hives[r5].SetTrigger(Flash);
 
-            StartCoroutine(StopHive());
+            //StartCoroutine(StopHive());
             
-            IEnumerator StopHive()
+            /*IEnumerator StopHive()
             {
                 yield return new WaitForSeconds(1f);
                 hives[r1].gameObject.SetActive(false);
@@ -42,13 +44,13 @@ namespace Hadal.UI
                 hives[r3].gameObject.SetActive(false);
                 hives[r4].gameObject.SetActive(false);
                 hives[r5].gameObject.SetActive(false);
-            }
+            }*/
         }
 
         [Button("Clear All")]
         public void ClearAllHive()
         {
-            hives.ForEach(o => o.gameObject.SetActive(false));
+            hives.ForEach(o => o.ResetTrigger(Flash));
         }
     }
 }
