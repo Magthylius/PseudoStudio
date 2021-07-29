566761ba5d83f90fe67298e86552a7137fa12f9e
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-29 15:51:26 +0800

    player ui update

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/Utilities/UIUtilitiesHandler.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/Utilities/UIUtilitiesHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Hadal.Networking;
 using UnityEngine;
 using TMPro;
 
@@ -8,10 +9,31 @@ namespace Hadal.UI
     public class UIUtilitiesHandler : MonoBehaviour
     {
         public UIGenericGaugeHandler FlareGauge;
+        
+        public UIGenericGaugeHandler SaviourGauge;
+        public UIGenericGaugeHandler HarpoonerGauge;
+        public UIGenericGaugeHandler InformerGauge;
+        public UIGenericGaugeHandler TrapperGauge;
 
-        public void SetupFlare(int amount)
+        public void Initialize(PlayerClassType classType)
         {
-            
+            switch (classType)
+            {
+                case PlayerClassType.Saviour: 
+                    SaviourGauge.gameObject.SetActive(true);
+                    break;
+                
+                case PlayerClassType.Harpooner: break;
+                
+                case PlayerClassType.Informer:
+                    InformerGauge.gameObject.SetActive(true);
+                    break;
+                
+                case PlayerClassType.Trapper:
+                    TrapperGauge.gameObject.SetActive(true);
+                    break;
+            }
         }
+        
     }
 }
