baa07266b9a8ad9d3bd29d57c63053a6c372fd90
Magthylius <56510076+Magthylius@users.noreply.github.com>
2021-07-30 22:36:54 +0800

    testing cleanup on cache

--- ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIContextHandler.cs
+++ ProjectHadal/Assets/_PROJECT/Scripts/User Interface/UIContextHandler.cs
@@ -2,26 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Hadal.UI
 {
     public class UIContextHandler : MonoBehaviour
     {
+        private void Start()
+        {
+            SetupTorpedoSalvage();
+        }
+
         [Header("Revive UI")] 
         [SerializeField] private Animator reviveAnimator;
-
         private static readonly int PlayerDownHash = Animator.StringToHash("PlayerDown");
         private static readonly int PlayerRevivedHash = Animator.StringToHash("PlayerRevived");
         
-        public void PlayerWentDown()
-        {
-            reviveAnimator.SetTrigger(PlayerDownHash);
-        }
-        
-        public void PlayerRevived()
-        {
-            reviveAnimator.SetTrigger(PlayerRevivedHash);
-        }
+        public void PlayerWentDown() => reviveAnimator.SetTrigger(PlayerDownHash);
+        public void PlayerRevived() => reviveAnimator.SetTrigger(PlayerRevivedHash);
         
         [Header("Jumpstart UI")]
         [SerializeField] private Animator jumpstartAnimator;
@@ -32,5 +30,23 @@ namespace Hadal.UI
         public void StartJumpstart() => jumpstartAnimator.SetTrigger(AllowJumpstart);
         public void SuccessJumpstart() => jumpstartAnimator.SetTrigger(JumpstartSucceed);
         public void FailJumpstart() => jumpstartAnimator.SetTrigger(JumpstartFailure);
+
+        [Header("Torpedo Salvage UI")] 
+        [SerializeField] private Image salvageLeftFiller;
+        [SerializeField] private Image salvageRightFiller;
+        [SerializeField] private Animator salvageAnimator;
+
+        void SetupTorpedoSalvage()
+        {
+            salvageLeftFiller.fillAmount = 0f;
+            salvageRightFiller.fillAmount = 0f;
+        }
+
+        void SetSalvageFillerRatio(float amount)
+        {
+            salvageLeftFiller.fillAmount = amount;
+            salvageRightFiller.fillAmount = amount;
+        }
+        
     }
 }
